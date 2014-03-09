2205a50d83ea1749841aad42ec9ba3e5d6676e3b
Taylor Friesen <tdfriesen@gmail.com>
2014-03-09 16:16:06 -0700

    removed table from surplus page

--- forms/surplus-new.php
+++ forms/surplus-new.php
@@ -330,7 +330,8 @@ jQuery.extend(Drupal.settings, {"basePath":"\u002Fis\u002F", "pathPrefix":"", "a
 		<input type='button' class='input_id_Item_Row' style='float:right' onclick="AddToList('id_Item_Row', 'Item_Return_Type', '', 'Item_Description', '', 'Inventory_ID', '', 'Serial_Number', ''); return false" value="Add Custom Item">
 		<br><br>
 
-		<table class='itemlisttable' border='0' cellspacing='0' style='width:100%' id='itemsTable'>
+		 <!-- 
+<table class='itemlisttable' border='0' cellspacing='0' style='width:100%' id='itemsTable'>
 			  <tr>	
 					<th width='8%'><a class="hasTooltip">Return</a><span>
 						<ul>Yes means that you would like the device returned to you after being wiped.</ul>
@@ -340,7 +341,7 @@ jQuery.extend(Drupal.settings, {"basePath":"\u002Fis\u002F", "pathPrefix":"", "a
 					<th width='37%'><a class="hasTooltip">Description</a><span>Enter a description or use the Items dropdown to select a pre-made item from the list.</span></th>
 					<th width='15%'><a class="hasTooltip">Inventory ID</a><span>If the device has an inventory ID number on it, please fill it in as it will help us to identify the device in our system.</span></th>
 					<th width='20%'><a class="hasTooltip">Serial Number</a><span>For devices that have a serial number, please enter the number in the field below.</span></th>
-					<!--<th width='14%'>Item Type</th>-->
+					<!~~ <th width='14%'>Item Type</th> ~~>
 					<th width='6%'><a class="hasTooltip"><img size='100%' src='images/delete16.png'></a><span>Click on the X next to an item to remove it from the list.</span></th>
 			  </tr>
 
@@ -348,29 +349,30 @@ jQuery.extend(Drupal.settings, {"basePath":"\u002Fis\u002F", "pathPrefix":"", "a
 		  for ($pc = 0; $pc < NUMROWS; ++$pc) {
 		?>
 			  <tr style='display:none;' id="id_Item_Row<?php echo $pc;?>">
-				<td style='width:8%'>
+				<!~~<td style='width:8%'>
 				  <select class='selector' style='width:100%' id="id_Item_Return_Type_<?php echo $pc;?>" name='Item_Return_Type[]'>
 					<option value="">&nbsp;</option>
 					<option value="Yes">Yes, return to me.</option>
 					<option value="No">Don't return to me.</option>
 				  </select>
-				</td>
+				</td>~~>
 				<td width='37%'><input type='text' style='width:95%' id="id_Item_Description_<?php echo $pc;?>" name='Item_Description[]' maxlength='54' class='requiredField'></td>
 				<td width='15%'><input type='text' style='width:90%' id="id_Inventory_ID_<?php echo $pc;?>" name='Inventory_ID[]' maxlength='30'></td>
 				<td width='20%'><input type='text' style='width:95%' id="id_Serial_Number_<?php echo $pc;?>" name='Serial_Number[]' maxlength='30'></td>
-				<!--<td width='14%'><input type='text' style='width:90%' id="id_Item_Type_<?php echo $pc;?>" name='Item_Type[]' maxlength='30'></td>-->
+				<!~~<td width='14%'><input type='text' style='width:90%' id="id_Item_Type_<?php echo $pc;?>" name='Item_Type[]' maxlength='30'></td>~~>
 				<td width='6%'><center><img class="removeItem" style='width:50%' src='images/delete16.png' onclick="RemoveFromList('id_Item_Row', <?php echo $pc;?>,'Item_Return_Type','Item_Description','Inventory_ID','Serial_Number')"/><center></td>
 			  </tr>
 			  
 		<?php } ?>
 		</table>
+ -->
 	</div>
 
 
 </div>
 
 
-<div class="form-item webform-component webform-component-textfield">
+<!-- <div class="form-item webform-component webform-component-textfield">
 	<label for="building-room">Pick up location (Building & Room):</label>
 	<input type="text" id="building-room" name="building-room" value="" size="70" class='requiredField'>
 </div>
@@ -382,9 +384,10 @@ jQuery.extend(Drupal.settings, {"basePath":"\u002Fis\u002F", "pathPrefix":"", "a
 	<div class="form-textarea-wrapper resizable">
 		<textarea id="notes" name="notes" cols="3" rows"5" class="form-textarea"></textarea>
 	</div>
-</div>
+</div> -->
 
 <div class="form-actions form-wrapper" id="edit-actions">
+
 <script language="javascript" type="text/javascript">
 function ajaxFunction(){
  var ajaxRequest;  // The variable that makes Ajax possible!
