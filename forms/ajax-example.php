83bfeaaffa19773a1db1f62ac119c62e26e7e784
Taylor Friesen <tdfriesen@gmail.com>
2014-03-09 17:38:39 -0700

    added more lines to ajax

--- forms/ajax-example.php
+++ forms/ajax-example.php
@@ -9,6 +9,7 @@ mysql_connect($dbhost, $dbuser, $dbpass) or die(mysql_error());
 mysql_select_db($dbname) or die(mysql_error());
 	// Retrieve data from Query String
 $queryoption = $_GET['queryoption'];
+$queryoption = mysql_real_escape_string($queryoption);
 echo "The query option is " . $queryoption . "<br />";
 if($queryoption == "max"){
 	$location = $_GET['location'];
